40c4fee0b380dde0d27d9ffaa8838f8b006b5064
Christopher MCAdams <mca.christopher@gmail.com>
2024-02-04 21:22:11 -0500

    wip

--- algos/index.js
+++ algos/index.js
@@ -2,15 +2,5 @@ const genSockData = require('./sortingSocks/utils/generateSockData');
 const genHaystack = require('./findNeedle/utils/generateHaystack');
 const sortingSocks = require('./sortingSocks');
 const isPalindrome = require('./isPalindrome');
-const {default: findNeedle, linearSearch, binarySearch} = require('./findNeedle')
 
-const palindrome = isPalindrome('racecar', 'poop', 'dog');
-const {data} = genSockData(24, 3);
-const socks = sortingSocks({ data });
-
-const needle = 2;
-const haystack = genHaystack(needle);
-const foundInHaystack =  findNeedle(needle,haystack );
-const findInHaystack2 = binarySearch(needle, haystack);
-
-Promise.all([palindrome, socks, foundInHaystack, findInHaystack2]).then(console.log);
+module.exports = { isPalindrome, sortingSocks };
