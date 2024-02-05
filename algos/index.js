b4f6e52902c7fe93053d50a10198b9a6f401ee33
Christopher MCAdams <mca.christopher@gmail.com>
2024-02-04 21:22:11 -0500

    wip

--- algos/index.js
+++ algos/index.js
@@ -1,16 +1,18 @@
+require('newrelic');
+
 const genSockData = require('./sortingSocks/utils/generateSockData');
 const genHaystack = require('./findNeedle/utils/generateHaystack');
 const sortingSocks = require('./sortingSocks');
 const isPalindrome = require('./isPalindrome');
-const {default: findNeedle, linearSearch, binarySearch} = require('./findNeedle')
+const { default: findNeedle, linearSearch, binarySearch } = require('./findNeedle')
 
 const palindrome = isPalindrome('racecar', 'poop', 'dog');
-const {data} = genSockData(24, 3);
+const { data } = genSockData(24, 3);
 const socks = sortingSocks({ data });
 
 const needle = 2;
 const haystack = genHaystack(needle);
-const foundInHaystack =  findNeedle(needle,haystack );
+const foundInHaystack = findNeedle(needle, haystack);
 const findInHaystack2 = binarySearch(needle, haystack);
 
 Promise.all([palindrome, socks, foundInHaystack, findInHaystack2]).then(console.log);
