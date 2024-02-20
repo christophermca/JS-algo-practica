8575cc6a202aab7fe06a25fb9f49ce44edacbe74
Christopher MCAdams <mca.christopher@gmail.com>
2024-02-19 21:05:06 -0500

    wip2

--- algos/index.js
+++ algos/index.js
@@ -1,6 +1,6 @@
-const genSockData = require('./sortingSocks/utils/generateSockData');
+const generateSockData = require('./sortingSocks/utils/generateSockData');
 const genHaystack = require('./findNeedle/utils/generateHaystack');
 const sortingSocks = require('./sortingSocks');
 const isPalindrome = require('./isPalindrome');
 
-module.exports = { isPalindrome, sortingSocks };
+module.exports = { isPalindrome, sortingSocks, generateSockData };
