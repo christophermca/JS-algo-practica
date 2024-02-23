9eda78c704316c07ccad447a327afb365c3b4222
Christopher MCAdams <mca.christopher@gmail.com>
2024-02-22 21:16:33 -0500

    wip

--- algos/sortingSocks/lib/generateSockData.js
+++ algos/sortingSocks/lib/generateSockData.js
@@ -1,32 +1,49 @@
-const data = [], log = {};
-function generateSockPile(amount = 0, colors = 1) {
-  while (data.length < amount) {
-    const rand = Math.round((Math.random() * colors));
-    let sock;
-    switch (rand) {
-      case 1:
-        sock = 'red'
-        break;
-      case 2:
-        sock = 'green'
-        break;
-      case 3:
-        sock = 'blue'
-    }
-
-    if (sock) {
-      _updateLog(sock);
-      data.push(sock);
-    }
+class GenerateSockPile {
+  constructor(amount, colors) {
+    console.log({ amount, colors })
+    this.colors = colors;
+    this.amount = amount;
+    this.initialize()
   }
-  return { log, data };
+
+  initialize() {
+    this.data = []
+    this.log = {}
+    this._build();
+  }
+
+  _updateLog(socks) {
+    if (typeof this.log[socks] !== 'number') {
+      this.log[socks] = 0;
+    }
+    ++this.log[socks];
+  }
+
+  _build() {
+    console.log('smile', { data: this.data, log: this.log })
+
+    while (this.data.length < this.amount) {
+      const rand = Math.round((Math.random() * this.colors));
+      let sock;
+      switch (rand) {
+        case 1:
+          sock = 'red'
+          break;
+        case 2:
+          sock = 'green'
+          break;
+        case 3:
+          sock = 'blue'
+      }
+
+      if (sock) {
+        this._updateLog(sock);
+        this.data.push(sock);
+      }
+    }
+    return { log: this.log, data: this.data };
+  }
 }
 
-function _updateLog(socks) {
-  if (typeof log[socks] !== 'number') {
-    log[socks] = 0;
-  }
-  ++log[socks];
-}
 
-module.exports = generateSockPile;
+module.exports = GenerateSockPile;
