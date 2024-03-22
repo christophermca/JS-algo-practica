8f2a89c1cb19470ef69baf2cb2b5a0e14a17ef85
Christopher MCAdams <mca.christopher@gmail.com>
2024-03-22 14:34:10 -0400

    docs: Adding README

---
+++ algos/findNeedle/README.md
@@ -0,0 +1,14 @@
+# Find Needle algo
+
+
+## Helpers
+
+_location: `bin/generateHaystack`_
+
+`GenerateHaystack<Haystack>(needle: string)`
+
+`findChar(data: string, key: number)`
+`linearSearch(data: string, key:  number)`
+`binarySearch(data: string, key: number)`
+
+Type Haystack = {haystack: string, length: number}
--- algos/index.js
+++ algos/index.js
@@ -1,6 +1,7 @@
 const GenerateSockPile = require('./sortingSocks/bin/generateSockData');
 const GenerateHaystack = require('./findNeedle/bin/generateHaystack');
 const { findChar, linearSearch, binarySearch } = require('./findNeedle');
+
 const sortingSocks = require('./sortingSocks');
 const isPalindrome = require('./isPalindrome');
 
